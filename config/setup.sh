d62dc0e53252df8d4360a123a4caf680a63ee5fd
sktrinh12 <sktrinh12@gmail.com>
2024-09-16 18:27:42 -0500

    add broker var to bash, handle dynaic broker, create stop script for cronjob

--- config/setup.sh
+++ config/setup.sh
@@ -1,42 +1,78 @@
 #!/bin/bash
 set -e
 CODE_NAME=$(cat code_name)
+BROKER=$(cat broker | tr '[:lower:]' '[:upper:]')
+rm broker
 rm code_name
-echo $CODE_NAME
+echo "CODE_NAME=${CODE_NAME}; BROKER=${BROKER}"
 apt-get update
-apt-get install -y software-properties-common
+apt-get install -y software-properties-common zip
 add-apt-repository -y ppa:deadsnakes/ppa
-apt-get install -y python3.10 python3.10-venv python3.10-dev zip
+apt-get install -y python3.10 python3.10-venv python3.10-dev
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 rm -rf aws
 rm awscliv2.zip
 
-# Switch to ubuntu user and install Poetry
+# install Poetry, download code files from s3
 sudo -u ubuntu -i bash << EOF
   export PATH="/home/ubuntu/.local/bin:/usr/local/bin:$PATH"
   curl -sSL https://install.python-poetry.org | python3 -
-  echo 'alias run="poetry run python main.py"' >> /home/ubuntu/.bashrc
+  echo 'alias run="poetry run python /home/ubuntu/${CODE_NAME}-main/main.py"' >> /home/ubuntu/.bashrc
 
   echo ".................................."
   echo "DOWNLOAD CODE FROM AWS S3..."
   echo ".................................."
 
   aws s3 cp s3://lumi-code/${CODE_NAME}-main.zip /home/ubuntu/
-  cd /home/ubuntu
+  cd /home/ubuntu/
   unzip "${CODE_NAME}-main.zip" -d .
   rm "${CODE_NAME}-main.zip"
-  cd "${CODE_NAME}-main"
+EOF
 
+# create a stop script
+cat <<EOF > /home/ubuntu/stop.sh
+  #!/bin/bash
+  PID=\$(pgrep -f "poetry run python /home/ubuntu/${CODE_NAME}-main/main.py")
+  if [ -n "\${PID}" ]; then
+    kill "\${PID}"
+    echo "Process with PID \${PID} has been killed."
+  else
+    echo "No process found for poetry run python main.py (${CODE_NAME})"
+  fi
+EOF
+
+if [ "$BROKER" == "TRADIER" ]; then
+  BROKER_LABEL_1="TRADIER_ACCESS_TOKEN"
+  BROKER_LABEL_2="TRADIER_ACCOUNT_NUMBER"
+else
+  BROKER_LABEL_1="${BROKER}_API_KEY"
+  BROKER_LABEL_2="${BROKER}_API_SECRET"
+fi
+
+echo ".................................."
+echo "BROKER LABELS_1: ${BROKER_LABEL_1}"
+echo "BROKER LABELS_2: ${BROKER_LABEL_2}"
+echo ".................................."
+
+chmod +x /home/ubuntu/stop.sh
+
+# fetch secrets from ssm, then install packages
+sudo -u ubuntu -i bash << EOF
+  export PATH="/home/ubuntu/.local/bin:/usr/local/bin:$PATH"
   echo ".................................."
   echo "FETCHING SECRETS FROM AWS SSM..."
   echo ".................................."
 
-  echo "ALPACA_API_KEY=\$(aws ssm get-parameter --name 'ALPACA_API_KEY' --with-decryption --query 'Parameter.Value' --output text)" >> .env
-  echo "ALPACA_API_SECRET=\$(aws ssm get-parameter --name 'ALPACA_API_SECRET' --with-decryption --query 'Parameter.Value' --output text)" >> .env
-  echo "ALPACA_IS_PAPER=True" >> .env
+  cd "${CODE_NAME}-main"
+  echo "${BROKER_LABEL_1}=$(aws ssm get-parameter --name "${BROKER_LABEL_1}" --with-decryption --query 'Parameter.Value' --output text)" >> .env
+  echo "${BROKER_LABEL_2}=$(aws ssm get-parameter --name "${BROKER_LABEL_2}" --with-decryption --query 'Parameter.Value' --output text)" >> .env
+  echo "${BROKER}_IS_PAPER=True" >> .env
   echo "IS_BACKTESTING=False" >> .env
+
+  # Add cron job to stop the process at 4:30 PM EST
+  (crontab -l 2>/dev/null; echo "30 16 * * * /home/ubuntu/stop.sh") | crontab -
 
   poetry lock
   poetry install
