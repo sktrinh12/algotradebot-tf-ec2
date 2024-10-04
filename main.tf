58c9e4604afe4cac20519436bca002a4e18675a5
sktrinh12 <sktrinh12@gmail.com>
2024-10-04 11:25:05 -0500

    remove quotes

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "tls_private_key" "private_key" {
 }
 
 resource "aws_key_pair" "lumiw_key" {
-  key_name   = "${var.key_name}_ec2_key"
+  key_name   = ${var.key_name}_ec2_key
   public_key = tls_private_key.private_key.public_key_openssh
 }
 
